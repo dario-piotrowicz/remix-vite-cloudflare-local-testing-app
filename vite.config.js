12a2c67cb96465e45af54af1b729f9db46e6418f
Dario Piotrowicz <dario@cloudflare.com>
2023-10-25 16:05:36 +0100

    add pages configs (WIP)

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [remix({
+    cloudflare: {
+      pages: {
+        // the bindings go here
+      }
+    }
+  })],
 });
