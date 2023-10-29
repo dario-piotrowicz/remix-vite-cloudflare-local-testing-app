b81c9953edde5066c4d7bb297daae285686eb6dc
Dario Piotrowicz <dario@cloudflare.com>
2023-10-29 12:04:31 +0000

    uncomment code that doesn't need to be commented out

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-// import type { LinksFunction } from "@remix-run/cloudflare";
-// import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction } from "@remix-run/cloudflare";
+import { cssBundleHref } from "@remix-run/css-bundle";
 import {
   Links,
   LiveReload,
@@ -9,9 +9,9 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-// export const links: LinksFunction = () => [
-//   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-// ];
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
 
 export default function App() {
   return (
@@ -20,7 +20,7 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
-        {/* <Links /> */}
+        <Links />
       </head>
       <body>
         <Outlet />
